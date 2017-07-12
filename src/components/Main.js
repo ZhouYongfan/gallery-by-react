0cff342b7e6118419d0a4cc467bfceaf293df131
zhouyongfan <892176893@qq.com>
2017-07-12 15:09:08 +0800

    增加图片旋转，修复hidden问题

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,9 +17,15 @@ imageDatas = ((imageDatasArr) => {
   return imageDatasArr;
 })(imageDatas);
 
+//获取区间内的一个随机值
 function getRangeRandom(low, high) {
   return Math.floor(Math.random() * (high - low) + low);
 };
+
+//获取0~30°之间的一个任意正负值
+function get30DegRandom() {
+  return Math.random() > 0.5 ? '' : '-' + Math.floor(Math.random() * 30);
+}
 
 class ImgFigure extends React.Component {
 
@@ -29,6 +35,14 @@ class ImgFigure extends React.Component {
     if(this.props.arrange.pos) {
       styleObj = this.props.arrange.pos;
     }
+
+    //如果图片的旋转角度有值并且不为0，添加旋转角度
+    if(this.props.arrange.rotate) {
+      (['-moz-', '-ms-', '-webkit-', '']).forEach((value) => {
+        styleObj[value + 'transform'] = 'rotate(' + this.props.arrange.rotate + 'deg)';
+      });
+    }
+
     return(
       <figure className="img-figure" style={styleObj}>
         <img src={this.props.data.imageURL} alt={this.props.data.title} />
@@ -46,12 +60,13 @@ class AppComponent extends React.Component {
     super();
     this.state = {
       imgsArrangeArr: [
-        // {
-        //   pos: {
-        //     left: '0',
-        //     top: '0'
-        //   }
-        // }
+        {
+          pos: {
+            left: '0',
+            top: '0'
+          },
+          rotate: 0   //旋转角度
+        }
       ]
     }
     this.Constant = {
@@ -70,35 +85,6 @@ class AppComponent extends React.Component {
       }
     }
   }
-
-  // getInitialState() {
-  //   return {
-  //     imgsArrangeArr: [
-  //       {
-  //         pos: {
-  //           left: '0',
-  //           top: '0'
-  //         }
-  //       }
-  //     ]
-  //   }
-  // }
-
-  // Constant = {
-  //   centerPos: {
-  //     left: 0,
-  //     right: 0
-  //   },
-  //   hPosRange: { //水平方向的取值范围
-  //     leftSecX: [0, 0],
-  //     rightSecX: [0, 0],
-  //     y: [0, 0]
-  //   },
-  //   vPosRange: { //垂直方向的取值范围
-  //     x: [0, 0],
-  //     topY: [0, 0]
-  //   }
-  // }
 
   /*
    *重新布局所有图片
@@ -125,15 +111,21 @@ class AppComponent extends React.Component {
     //首先居中centerIndex的图片
     imgsArrangeCenterArr[0].pos = centerPos;
 
+    //居中的 centerIndex 的图片不需要旋转
+    imgsArrangeCenterArr[0].rotate = 0;
+
     //取出要布局上侧的图片的状态信息
     topImgSpliceIndex = Math.floor(Math.random() * (imgsArrangeArr.length - topImgNum));
     imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
 
     //布局位于上侧的图片
     imgsArrangeTopArr.forEach((value, index) => {
-      imgsArrangeTopArr[index].pos = {
-        top: getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]),
-        left: getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+      imgsArrangeTopArr[index] = {
+        pos: {
+          top: getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]),
+          left: getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+        },
+        rotate: get30DegRandom()
       }
     })
 
@@ -147,10 +139,13 @@ class AppComponent extends React.Component {
         hPosRangeLORX = hPosRangeRightSecX;
       }
 
-      imgsArrangeArr[i].pos = {
-        top: getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
-        left: getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
-      }
+      imgsArrangeArr[i] = {
+        pos: {
+          top: getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
+          left: getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
+        },
+        rotate: get30DegRandom()
+      };
     }
 
     if(imgsArrangeTopArr && imgsArrangeTopArr[0]) {
@@ -209,12 +204,12 @@ class AppComponent extends React.Component {
           pos: {
             left: 0,
             top: 0
-          }
+          },
+          rotate: 0
         }
       }
       imgFigures.push(<ImgFigure data={element} ref={'imgFigure' + index} arrange={this.state.imgsArrangeArr[index]} key={index} />);
     });
-    console.log(imgFigures[0].ref)
     return (
       <section className="stage" ref="stage">
         <section className="img-sec">
